b9312891efd284fd9a241546454641b3b6ad138c
SamuelSchefers <scheferssamuel@gmail.com>
2026-01-16 15:48:49 +0000

    jan16th

---
+++ Assignments/BooleanQuiz.py
@@ -0,0 +1,8 @@
+print("Answer all 5 questions with either 'True' or 'False'")
+Answer_1=input(":")
+Answer_1Bool=bool(Answer_1)
+
+Answer_2=input("?:")
+Answer_3=input("?:")
+Answer_4=input("?:")
+Answer_5=input("?:")
