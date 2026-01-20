720977ebe56e4ca20f5dc447404c33fabed957e0
SamuelSchefers <scheferssamuel@gmail.com>
2026-01-20 09:33:18 -0600

    jan 20th

--- Assignments/BooleanQuiz.py
+++ Assignments/BooleanQuiz.py
@@ -1,8 +1,26 @@
 print("Answer all 5 questions with either 'True' or 'False'")
-Answer_1=input(":")
+#Answer key: 1=True 2=True 3=False 4=False 5=True
+Answer_1=input("George Boolean was a British mathmetician: ")
 Answer_1Bool=bool(Answer_1)
+if Answer_1Bool=true
+else
 
-Answer_2=input("?:")
-Answer_3=input("?:")
-Answer_4=input("?:")
-Answer_5=input("?:")
+Answer_2=input(": ")
+Answer_2Bool=bool(Answer_2)
+if Answer2_Bool=true
+else
+
+Answer_3=input("A Boolean expression requires the data type to be in string format: ")
+Answer_3Bool=bool(Answer_3)
+if Answer3_Bool=false
+else
+
+Answer_4=input("'=' is the comparison operator for 'Equal to': ")
+Answer_4Bool=bool(Answer_4)
+if Answer_4Bool=false
+else
+
+Answer_5=input("The : ")
+Answer5_Bool=bool(Answer_5)
+if Answer5_Boo=true
+else
