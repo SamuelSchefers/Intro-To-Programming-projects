59ff25ab39d76852c3c6c396b58b7aff3fb264ff
SamuelSchefers <scheferssamuel@gmail.com>
2026-01-27 15:16:30 +0000

    finally got the quiz to work 1/27

--- Assignments/BooleanQuiz.py
+++ Assignments/BooleanQuiz.py
@@ -1,39 +1,43 @@
-print("Answer all 5 questions with either 'True' or 'False'")
+print("Answer all 5 questions with either 'True' or 'False")
 #Answer key: 1=True 2=True 3=False 4=False 5=True
 Question_1=input("George Boolean was a British mathematician: ")
-Question_1_bool=bool(Question_1)
-Answer_1 = True
-if Question_1_bool==True:
-    print("Correct")
-elif Question_1==False:
-    print("Incorrect")
+if Question_1.lower()=="true":
+    print("Correct") 
+elif Question_1.lower()=="false":
+    print("Incorrect") 
 else:
     print("Invalid response, please answer with either 'True' or 'False'")
 
 Question_2=input("Boolean expressions use 'True' or 'False': ")
-Answer_2Bool=bool(Question_2)
-if Answer_2Bool==True:
+if Question_2.lower()=="true":
     print("Correct")
-else:
+elif Question_2.lower()=="false":
     print("Incorrect")
+else:
+    print("Invalid response, please answer with either 'True' or 'False'")
+
 
 Question_3=input("A Boolean expression requires the data type to be in string format: ")
-Answer_3Bool=bool(Question_3)
-if Answer_3Bool==False:
+if Question_3.lower()=="false":
     print("Correct")
-else:
+elif Question_3.lower()=="true":
     print("Incorrect")
+else:
+    print("Invalid response, please answer with either 'True' or 'False'")
 
 Question_4=input("'=' is the comparison operator for 'Equal to': ")
-Answer_4Bool=bool(Question_4)
-if Answer_4Bool==False:
+if Question_4.lower()=="false":
     print("Correct")
-else:
+elif Question_4.lower()=="true":
     print("Incorrect")
+else:
+    print("Invalid response, please answer with either 'True' or 'False'")
 
-Answer_5=input("The 'True' and 'False' are case sensitive: ")
-Answer5_Bool=bool(Answer_5)
-if Answer5_Bool==True:
+
+Question_5=input("The 'True' and 'False' are case sensitive: ")
+if Question_5.lower()=="true":
     print("Correct")
-else:
+elif Question_5.lower()=="false":
     print("Incorrect")
+else:
+    print("Invalid response, please answer with either 'True' or 'False'")
