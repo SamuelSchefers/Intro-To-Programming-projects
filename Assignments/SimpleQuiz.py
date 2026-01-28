2fb84ebd20d7c6ccc4fa641ed721b2c9d9d26c1c
SamuelSchefers <scheferssamuel@gmail.com>
2026-01-28 15:28:16 +0000

    January 28th

---
+++ Assignments/SimpleQuiz.py
@@ -0,0 +1,17 @@
+total_score=0
+question_1=input("What is the movie that came out in 2001 and stars an ogre and a donkey as its main characters?: ") #shrek
+question_2=input("What is the capital of Canada?: ") #ottawa
+question_3=input("?: ")
+question_4=input("?: ")
+question_5=input("?: ")
+question_6=input("?: ")
+#hardest part of this assignment is coming up with questions because im suuper creative
+print("Your final score is " + str(total_score) + "/6")
+if total_score==6:
+    print("A perfect score! Great job!")
+#test_variable=input("a:")
+#if test_variable.lower()=="a":
+#    total_score += 1
+#else:
+#    print("How did you already manage to mess up your code")
+#print (total_score)
