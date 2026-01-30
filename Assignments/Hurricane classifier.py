e427046bb644e4c1d35647b34d46e2c0eda98b48
SamuelSchefers <scheferssamuel@gmail.com>
2026-01-30 15:45:44 +0000

    3rd commit on jan 30th

---
+++ Assignments/Hurricane classifier.py
@@ -0,0 +1,9 @@
+Windspeed=input("Seems like a storm is blowing in! Please enter the wind speed of the storm to classify it: ")
+print("You entered "+Windspeed+" MPH, if this is incorrect, please restart the program!")
+if
+#Tropical storm < 74 MPH
+#Category 1 < 96 MPH
+#Category 2 < 111 MPH
+#Category 3 < 130 MPH
+#Category 4 < 157 MPH
+#Category 5 >=157 MPH
