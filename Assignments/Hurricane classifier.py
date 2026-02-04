f38dc95811f6e0049158e054a9c39e0fc1cc9f5d
SamuelSchefers <scheferssamuel@gmail.com>
2026-02-04 15:35:48 +0000

    Hurricane classiier

--- Assignments/Hurricane classifier.py
+++ Assignments/Hurricane classifier.py
@@ -1,14 +1,15 @@
 Windspeed=input("Seems like a storm is blowing in! Please enter the wind speed of the storm to classify it: ")
 print("You entered "+Windspeed+" MPH, if this is incorrect, please restart the program!")
-if Windspeed<74:
+Windspeed_Float=float(Windspeed)
+if Windspeed_Float<74:
     print("It's a tropical storm!")
-elif Windspeed>=74 and Windspeed<96:
+elif Windspeed_Float>=74 and Windspeed_Float<96:
     print("It's a Category 1 hurricane!")
-elif Windspeed>=96 and Windspeed<111:
+elif Windspeed_Float>=96 and Windspeed_Float<111:
     print("It's a Category 2 hurricane!")
-elif Windspeed>=111 and Windspeed<130:
+elif Windspeed_Float>=111 and Windspeed_Float<130:
     print("It's a Category 3 hurricane!")
-elif Windspeed>=130 and Windspeed<157:
+elif Windspeed_Float>=130 and Windspeed_Float<157:
     print("It's a Category 4 hurricane!")
 else:
     print("ITS A CATEGORY 5 HURRICANE, TAKE SHELTER IMMEDIATELY!")
