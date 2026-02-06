e3637e7b88eae7aca28c91fb4b5392dd9c12434f
SamuelSchefers <scheferssamuel@gmail.com>
2026-02-06 15:48:27 +0000

    fortun

---
+++ Assignments/FortuneTeller.py
@@ -0,0 +1,8 @@
+print("Hello user! I am a magical fortune teller")
+try:
+    Initiation=input("Would you like me to tell you your fortune?").strip().lower*()
+except #Some sort of error here
+#Im out here placing blocks and #### because im in minecraft (Running out of time today)
+else
+#This runs assuming the try part of the code works and this breaks the loop
+#And continuing this absolutely god awful, no good, absolutely dreadful code
