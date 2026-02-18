988966f170016fbf7c709f7a717dc85e338dd937
SamuelSchefers <scheferssamuel@gmail.com>
2026-02-18 15:44:09 +0000

    bro figured out how to do exceptions

--- Assignments/FortuneTeller.py
+++ Assignments/FortuneTeller.py
@@ -3,28 +3,39 @@ def question_1():
     Answer1=(input("What is your lucky number? "))
     try:
         float(Answer1)
-    except TypeError:
-        print("TypeError, Please enter a valid number.")
+    except TypeError: #This just didnt ever come up for some reason, idk what it does tbh
+        print("TypeError, Please enter a valid number!")
+        question_1()
     except ValueError:
-        print("Value error, Please enter a number. ")
+        print("Value error, Please enter a number!") #Error shows up when you answer the question as a not number
+        question_1() #Loops function back
     else:
-        print("Else part test")
-        str(Answer1) #Remove this line, was using it 
-        #to test if it would allow me to print the answer from this function
-        #it didnt.
-        #We're just going to pretend it isnt an issue right now
+        #breaks loop when the answer is valid, returns it and hopefully doesnt break stuff?
         return Answer1
 def question_2():
-    Answer2=float(input("How many years into the future would you like to see? "))
+    Answer2=(input("How many years into the future would you like to see? "))
+    try:
+        float(Answer2)
+    except ValueError:
+        print("Value error, Please enter a number!")
+        question_2()
+    else:
+        return Answer2
 def question_3():
-    Answer3=float(input("Give me another magical number! "))
-#remember to int() and float() your question values, or else this piece of shitware will break
+    Answer3=(input("Give me another magical number! "))
+    try:
+        float (Answer3)
+    except ValueError:
+        print("Value error, Please enter a valid number!")
+        question_3()
+    else:
+        return Answer3
+#remember to int() and float() your question values
 print("Hello user! I am a magical fortune teller")
 def fortune_teller():
         Initiation=input("Would you like me to tell you your fortune? ").strip().lower()
         if Initiation=="yes":
             print("Perfect! Allow me to ask you 3 simple questions and I shall determine your fortune!")
-            return Initiation
         elif Initiation=="no":
             print("Oh... well that's too bad!")
             quit()
@@ -34,7 +45,8 @@ def fortune_teller():
 fortune_teller() # Actually makes the function run
 print("The experiment with the glowing cats") #yes im going to keep using this as a placeholder to tell when parts ot the code run correctly
 question_1()
-print(question_1)
+question_2()
+question_3()
 #writing the 5 possible fortunes here
 #"I forsee a terrible accident at sea that leaves you and 3 others stranded on a deserted island forever, never to be rescued!"
 #"I forsee a grand future full of love and happiness, and immense wealth beyond your wildest dreams!"
