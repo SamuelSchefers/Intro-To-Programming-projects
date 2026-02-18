f4e9254eb22e3ca28487a1ab2379f650125b760b
SamuelSchefers <scheferssamuel@gmail.com>
2026-02-18 15:03:06 +0000

    February 18th

--- Assignments/FortuneTeller.py
+++ Assignments/FortuneTeller.py
@@ -1,21 +1,27 @@
 import random
 def question_1():
-    Answer1=float(input("What is your lucky number?"))
-    while True:
-        try:
-            if Answer1 is float:
-                break #please break the try except block, and not my code... this should send it down to the return part and work
-        except TypeError:
-            print("Please enter a number.")
-    return Answer1
+    Answer1=(input("What is your lucky number? "))
+    try:
+        float(Answer1)
+    except TypeError:
+        print("TypeError, Please enter a valid number.")
+    except ValueError:
+        print("Value error, Please enter a number. ")
+    else:
+        print("Else part test")
+        str(Answer1) #Remove this line, was using it 
+        #to test if it would allow me to print the answer from this function
+        #it didnt.
+        #We're just going to pretend it isnt an issue right now
+        return Answer1
 def question_2():
-    Answer2=float(input("How many years into the future would you like to see?"))
+    Answer2=float(input("How many years into the future would you like to see? "))
 def question_3():
-    Answer3=float(input("Give me another magical number!"))
+    Answer3=float(input("Give me another magical number! "))
 #remember to int() and float() your question values, or else this piece of shitware will break
 print("Hello user! I am a magical fortune teller")
 def fortune_teller():
-        Initiation=input("Would you like me to tell you your fortune?").strip().lower()
+        Initiation=input("Would you like me to tell you your fortune? ").strip().lower()
         if Initiation=="yes":
             print("Perfect! Allow me to ask you 3 simple questions and I shall determine your fortune!")
             return Initiation
@@ -24,11 +30,11 @@ def fortune_teller():
             quit()
         else:
             print("Please answer with yes or no!")
-            fortune_teller() #loops back if invalid input... im not using whatever try except bs for this when this works "perfectly" fine
+            fortune_teller() #loops back if invalid input... works somehow
 fortune_teller() # Actually makes the function run
 print("The experiment with the glowing cats") #yes im going to keep using this as a placeholder to tell when parts ot the code run correctly
-#fun fact, there was an experiment with cat genetics where they were testing if cat genes were hereditary or not, and to test they glued bioluminescent genes to the cats genes they were testing and then if the kittens were glowing then they knew the genes were hereditary. The print function in with this line is a reference to that experiment because its how i tell if my code works.
-#I was on 4 hours of sleep when i first wrote this, genuinely what does ANY of this mean
+question_1()
+print(question_1)
 #writing the 5 possible fortunes here
 #"I forsee a terrible accident at sea that leaves you and 3 others stranded on a deserted island forever, never to be rescued!"
 #"I forsee a grand future full of love and happiness, and immense wealth beyond your wildest dreams!"
