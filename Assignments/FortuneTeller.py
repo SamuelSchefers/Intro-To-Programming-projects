efa5c3e0d08a7280089e26574a314bddc15681ec
SamuelSchefers <scheferssamuel@gmail.com>
2026-02-19 15:42:35 +0000

    Fortune teller finished

--- Assignments/FortuneTeller.py
+++ Assignments/FortuneTeller.py
@@ -43,17 +43,24 @@ def fortune_teller(): #function will loop itself until the user answers yes or n
             print("Please answer with yes or no!")
             fortune_teller() #loops back if invalid input... works somehow
 fortune_teller() # Actually makes the function run
-question_1() #the 3 questions run after the user enters yes and breaks the loop
-question_2() #without terminating the program
-question_3() #probably janky, but it works.
-Random_Number=random.randint(1,6)
-print("testing" + str(Random_Number))
-Fortune_Calculation=#deal with ts later, divide q2/q1 and q3 by random, add the results
-#writing the 5 possible fortunes here
-#"I forsee a terrible accident at sea that leaves you and 3 others stranded on a deserted island forever, never to be rescued!"
-#"I forsee a grand future full of love and happiness, and immense wealth beyond your wildest dreams!"
-#"I am terribly sorry but I forsee a great swarm of locusts descending upon your land and devouring all of your crops, leaving you with nothing but a barren wasteland and a lifetime of regret"
-#"I forsee a future where you will have immense wealth but will never find true love"
-#"I forsee a mediocre life, with a stable job, a loving family, and a small house in the suburbs"
+q1 = question_1() #the 3 questions run after the user enters yes and breaks the loop
+q2 = question_2() #without terminating the program
+q3 = question_3() #probably janky, but it works.
 
+Random_Number=random.randint(1,6)
+print(str(Random_Number))#for testing purposes
+Fortune_Calculation= float(q2)/float(q1) + float(q3)/float(Random_Number)
+print("Calculated number, TESTING")
+print(str(Fortune_Calculation))
+float_calculation=float(Fortune_Calculation)
+if float_calculation<5:
+    print("I forsee a terrible accident at sea that leaves you and 3 others stranded on a deserted island forever, never to be rescued!")
+elif float_calculation>=5 and float_calculation<10:
+    print("I forsee a grand future full of love and happiness, and immense wealth beyond your wildest dreams!")
+elif float_calculation>=10 and float_calculation<15:
+    print("I am terribly sorry but I forsee a great swarm of locusts descending upon your land and devouring all of your crops, leaving you with nothing but a barren wasteland and a lifetime of regret")
+elif float_calculation>=15 and float_calculation<20:
+    print("I forsee a future where you will have immense wealth but will never find true love")
+else:
+    print("I forsee a mediocre life, with a stable job, a loving family, and a small house in the suburbs")
 #remember that you can use the return function to return a value from a function, so you can use that in the randomizer
