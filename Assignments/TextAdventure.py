561ed3e87e825d522ff6dd017239027f06f3209d
SamuelSchefers <scheferssamuel@gmail.com>
2026-02-20 15:48:42 +0000

    text adventure project

---
+++ Assignments/TextAdventure.py
@@ -0,0 +1,8 @@
+print("Welcome to the text adventure game")
+print("To play the game, please answer each prompt with a number, such as '1' or '2'")
+print("Good luck!")
+print("_______________________________________________________________________________")
+print("The zombie apocalypse has started and you are in a overrun facility")
+print("You are currently safe in the room you are in")
+print("You can hear soldiers on the street outside, and you know there is a helicopter on the roof")
+#def game_start
