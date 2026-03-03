19030804d94bbc8d6ae28f65ea5d61660ae7cadd
SamuelSchefers <scheferssamuel@gmail.com>
2026-03-03 15:42:33 +0000

    for loops again

--- Practice/ForLoop.py
+++ Practice/ForLoop.py
@@ -1,4 +1,13 @@
 import time
-for i in range(11):
-    time.sleep(1)
-    print(i)
+for variable_1 in range(11):
+    time.sleep(.25)
+    print(variable_1)
+numberslist=[5,3,6,1,6]
+numberslisttotal=0
+for numbers in numberslist:
+    numberslisttotal += numbers
+print(numberslisttotal)
+sequence=[1,2,3,4,5,6]
+for squares in sequence:
+    total=squares**2
+print(total)
